aed5b616f5c5f60c5a6a13584f4c1808b66b2ef2
Andreas Fett <a.fett@gmx.de>
2025-11-19 20:15:00 +0100

    update copyright notice

--- include/argvcc.h
+++ include/argvcc.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2018, 2019 Andreas Fett
+   Copyright (c) 2018 - 2025 Andreas Fett
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
--- src/argv.cc
+++ src/argv.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2018, 2019 Andreas Fett. All rights reserved.
+   Copyright (c) 2018 - 2025 Andreas Fett. All rights reserved.
    Use of this source code is governed by a BSD-style
    license that can be found in the LICENSE file.
 */
